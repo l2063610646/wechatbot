728305d358e754da53abe6d25a22eb6999083aa8
全糖咖啡 <2063610646@qq.com>
2023-04-20 23:21:08 +0800

    feat(gtp.go)：新增对gpt-3.5-turbo模型的数据处理

--- gtp/gtp.go
+++ gtp/gtp.go
@@ -25,14 +25,19 @@ type ChatGPTResponseBody struct {
 }
 
 type ChoiceItem struct {
-	Text         string `json:"text"`
-	Index        int    `json:"index"`
-	Logprobs     int    `json:"logprobs"`
-	FinishReason string `json:"finish_reason"`
+	Text         string            `json:"text"`
+	Index        int               `json:"index"`
+	Logprobs     int               `json:"logprobs"`
+	FinishReason string            `json:"finish_reason"`
+	Message      ChoiceItemMessage `json:"message"`
 }
 
-// ChatGPTRequestBody 响应体
-type ChatGPTRequestBody struct {
+type ChoiceItemMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type OpenAIRequestBody struct {
 	Model            string  `json:"model"`
 	Prompt           string  `json:"prompt"`
 	MaxTokens        int     `json:"max_tokens"`
@@ -42,13 +47,36 @@ type ChatGPTRequestBody struct {
 	PresencePenalty  int     `json:"presence_penalty"`
 }
 
-// Completions gtp文本模型回复
-//curl https://api.openai.com/v1/completions
-//-H "Content-Type: application/json"
-//-H "Authorization: Bearer your chatGPT key"
-//-d '{"model": "text-davinci-003", "prompt": "give me good song", "temperature": 0, "max_tokens": 7}'
+type ChatGPTRequestBody struct {
+	Messages    []ChatGPTMessage `json:"messages"`
+	Temperature float64          `json:"temperature"`
+	MaxTokens   int              `json:"max_tokens"`
+	TopP        int              `json:"top_p"`
+	Model       string           `json:"model"`
+}
+
+type ChatGPTMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
 func Completions(msg string) (string, error) {
-	requestBody := ChatGPTRequestBody{
+	if config.LoadConfig().UseGPT {
+		//使用gptTurbo的模型
+		return ChatGPTCompletions(msg)
+	} else {
+		//使用text-davinci-003的模型
+		return OpenAICompletions(msg)
+	}
+}
+
+// OpenAICompletions gtp文本模型回复
+// curl https://api.openai.com/v1/completions
+// -H "Content-Type: application/json"
+// -H "Authorization: Bearer your chatGPT key"
+// -d '{"model": "text-davinci-003", "prompt": "give me good song", "temperature": 0, "max_tokens": 7}'
+func OpenAICompletions(msg string) (string, error) {
+	requestBody := OpenAIRequestBody{
 		Model:            "text-davinci-003",
 		Prompt:           msg,
 		MaxTokens:        1024,
@@ -104,6 +132,67 @@ func Completions(msg string) (string, error) {
 	var reply string
 	if len(gptResponseBody.Choices) > 0 {
 		reply = gptResponseBody.Choices[0].Text
+	}
+	log.Printf("gpt response text: %s \n", reply)
+	return reply, nil
+}
+
+func ChatGPTCompletions(msg string) (string, error) {
+	var message []ChatGPTMessage
+	err := json.Unmarshal([]byte(msg), &message)
+	requestBody := ChatGPTRequestBody{
+		Model:       "gpt-3.5-turbo",
+		Messages:    message,
+		MaxTokens:   1024,
+		Temperature: 0.7,
+		TopP:        1,
+	}
+	requestData, err := json.Marshal(requestBody)
+	if err != nil {
+		return "", err
+	}
+	log.Printf("request gtp json string : %v", string(requestData))
+	req, err := http.NewRequest("POST", BASEURL+"chat/completions", bytes.NewBuffer(requestData))
+	if err != nil {
+		return "", err
+	}
+
+	apiKey := config.LoadConfig().ApiKey
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+	client := &http.Client{}
+	if config.LoadConfig().UseProxy {
+		log.Printf("已开启代理 : %v", config.LoadConfig().ProxyUrl)
+		proxy, _ := url.Parse(config.LoadConfig().ProxyUrl)
+		client = &http.Client{
+			Transport: &http.Transport{
+				Proxy: http.ProxyURL(proxy),
+			},
+		}
+	}
+	response, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		return "", errors.New(fmt.Sprintf("gtp api status code not equals 200,code is %d", response.StatusCode))
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	gptResponseBody := &ChatGPTResponseBody{}
+	log.Println(string(body))
+	err = json.Unmarshal(body, gptResponseBody)
+	if err != nil {
+		return "", err
+	}
+
+	var reply string
+	if len(gptResponseBody.Choices) > 0 {
+		reply = gptResponseBody.Choices[0].Message.Content
 	}
 	log.Printf("gpt response text: %s \n", reply)
 	return reply, nil
